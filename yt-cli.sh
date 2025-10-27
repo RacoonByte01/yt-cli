d6703db6600120898c1334913526f09a2a4f37dc
RacoonByte01 <javier212002@gmail.com>
2025-10-27 13:48:28 +0100

    feat(type_media_search_menu, subscribed_menu): added a menu of the type of content you want to view from a selected channel. - Added a content type menu - Modified the subscribed channels menu for this function - Changed the quotation style of the main menu

--- yt-cli.sh
+++ yt-cli.sh
@@ -7,17 +7,27 @@ menu(){
 	echo -en "$1" | sh -c "fzf $2" | awk -F: '{printf $1}'
 }
 
+type_media_search_menu()
+{
+	menu "videos\nplaylists\nstreams\nshorts" "--header 'What content do you want?'"
+}
+
 subscribed_menu(){
 	[[ -f $PATH_SUBSCRIBED_CHANNELS ]] || touch $PATH_SUBSCRIBED_CHANNELS
-	SUBSCRIBED_CHANNEL_SELECTED=-1
-	SUBSCRIBED_CHANNELS=$SUBSCRIBED_CHANNELS$(echo "0: <-- Return";cat $PATH_SUBSCRIBED_CHANNELS)
-	# cat $PATH_SUBSCRIBED_CHANNELS | fzf --header "Select channel to see or add them in path=$PATH_SUBSCRIBED_CHANNELS"
-	menu "$SUBSCRIBED_CHANNELS" "--header \"Select channel to see or add them in path=$PATH_SUBSCRIBED_CHANNELS\""
+	local SUBSCRIBED_CHANNEL_SELECTED=-1
+	local TYPE_MEDIA_SEARCH_SELECTED=-1
+	local SUBSCRIBED_CHANNELS=$SUBSCRIBED_CHANNELS$(echo "0: <-- Return";cat $PATH_SUBSCRIBED_CHANNELS)
+	while [[ "$SUBSCRIBED_CHANNEL_SELECTED" != "0" ]]; do
+		SUBSCRIBED_CHANNEL_SELECTED=$(menu "$SUBSCRIBED_CHANNELS" "--header \"Select channel to see or add them in path=$PATH_SUBSCRIBED_CHANNELS\"")
+		if [[ "$SUBSCRIBED_CHANNEL_SELECTED" != "0" ]]; then
+			TYPE_MEDIA_SEARCH_SELECTED=$(type_media_search_menu)
+		fi
+	done
 }
 
 main_menu()
 {
-	shift; menu '0: News!\n1: Subscribed\n2: Stored\n3: Settings\n4: Close' "--header 'Select an option' --preview='figlet -f ANSI\ Shadow Hi again :3'"
+	shift; menu "0: News!\n1: Subscribed\n2: Stored\n3: Settings\n4: Close" "--header 'Select an option' --preview='figlet -f ANSI\ Shadow Hi again :3'"
 }
 
 while [[ "$MAIN_MENU" -ne "4" ]]; do

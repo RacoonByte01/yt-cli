1afd8c7b375020e5fc3e45331c5f4c26bca767ad
RacoonByte01 <javier212002@gmail.com>
2025-10-27 15:29:50 +0100

    feat(get_youtube playlists): now the lists of subscribed channels can be opened. - changes to playlist loading - adding some icons

--- yt-cli.sh
+++ yt-cli.sh
@@ -12,9 +12,14 @@ menu()
 get_youtube()
 {
 	if [[ $1 -eq 0 ]]; then
-		(echo "0: <-- Return"; yt-dlp --flat-playlist "$BASE$2/$3" --print "%(playlist_index)d: %(title)s") | fzf --header "Select the desired $3"
+		NUM_SELECTED=$(( echo "󰌑 Return"; yt-dlp --flat-playlist "$BASE$2/$3" --print "%(playlist_index)d: %(title)s" ) | fzf --header "Select the desired $3" | awk -F: '{print $1}')
+		if [[ $NUM_SELECTED == "󰌑 Return" ]]; then
+			echo "󰌑 Return"
+		else
+			yt-dlp --flat-playlist "$BASE$2/$3" --print url --playlist-items $NUM_SELECTED
+		fi
 	else
-		echo
+		( echo "󰌑 Return"; echo "󰐑 See all"; echo "󰲹 Music mode all"; echo "󰇚 Download all"; echo "󰐒 Save List"; yt-dlp --flat-playlist "$2" --print "%(playlist_index)d: %(title)s" ) | fzf --header "Select the desired videos"
 	fi
 }
 
@@ -28,13 +33,19 @@ subscribed_menu()
 	[[ -f $PATH_SUBSCRIBED_CHANNELS ]] || touch $PATH_SUBSCRIBED_CHANNELS
 	local SUBSCRIBED_CHANNEL_SELECTED=-1
 	local TYPE_MEDIA_SEARCH_SELECTED=-1
-	local SUBSCRIBED_CHANNELS=$SUBSCRIBED_CHANNELS$(echo "0: <-- Return";cat $PATH_SUBSCRIBED_CHANNELS)
-	while [[ "$SUBSCRIBED_CHANNEL_SELECTED" != "0" ]]; do
+	local SUBSCRIBED_CHANNELS=$SUBSCRIBED_CHANNELS$(echo "󰌑 Return";cat $PATH_SUBSCRIBED_CHANNELS)
+	while [[ "$SUBSCRIBED_CHANNEL_SELECTED" != "󰌑 Return" ]]; do
 		SUBSCRIBED_CHANNEL_SELECTED=$(menu "$SUBSCRIBED_CHANNELS" "--header \"Select channel to see or add them in path=$PATH_SUBSCRIBED_CHANNELS\"")
-		if [[ "$SUBSCRIBED_CHANNEL_SELECTED" != "0" ]]; then
+		if [[ "$SUBSCRIBED_CHANNEL_SELECTED" != "󰌑 Return" ]]; then
 			TYPE_MEDIA_SEARCH_SELECTED=$(type_media_search_menu)
 			if [[ "$TYPE_MEDIA_SEARCH_SELECTED" == playlists ]]; then
-				shift; get_youtube 0 $SUBSCRIBED_CHANNEL_SELECTED $TYPE_MEDIA_SEARCH_SELECTED
+				local URL_PLAYLIST=-1
+				while [[ "$URL_PLAYLIST" != "󰌑 Return" ]]; do
+					URL_PLAYLIST=$(shift; get_youtube 0 $SUBSCRIBED_CHANNEL_SELECTED $TYPE_MEDIA_SEARCH_SELECTED)
+					if [[ "$URL_PLAYLIST" != "󰌑 Return" ]]; then
+						shift; get_youtube 1 "$URL_PLAYLIST"
+					fi
+				done
 			fi
 		fi
 		
@@ -43,7 +54,7 @@ subscribed_menu()
 
 main_menu()
 {
-	shift; menu "0: News!\n1: Subscribed\n2: Stored\n3: Settings\n4: Close" "--header 'Select an option'"
+	shift; menu "0:  News!\n1: 󰵀 Subscribed\n2:  Stored\n3:  Settings\n4:  Close" "--header 'Select an option'"
 }
 
 while [[ "$MAIN_MENU" -ne "4" ]]; do

e1dfb1c4b95b88c06615f2006a86f0c4784b11ae
RacoonByte01 <javier212002@gmail.com>
2025-10-27 16:53:33 +0100

    feat(videos): adding basic functionality to videos, shorts, and streams

--- yt-cli.sh
+++ yt-cli.sh
@@ -6,26 +6,29 @@ BASE='https://www.youtube.com/@'
 
 menu()
 {
-	echo -en "$1" | sh -c "fzf $2" | awk -F: '{printf $1}'
+	echo -en "$1" | sh -c "fzf --style full $2" | awk -F: '{printf $'$3'}'
 }
 
 get_youtube()
 {
 	if [[ $1 -eq 0 ]]; then
-		NUM_SELECTED=$(( echo "󰌑 Return"; yt-dlp --flat-playlist "$BASE$2/$3" --print "%(playlist_index)d: %(title)s" ) | fzf --header "Select the desired $3" | awk -F: '{print $1}')
+		NUM_SELECTED=$(( echo "󰌑 Return"; yt-dlp --flat-playlist "$BASE$2/$3" --print "%(playlist_index)d: %(title)s" ) | fzf --style full --header "Select the desired $3" | awk -F: '{print $1}')
 		if [[ $NUM_SELECTED == "󰌑 Return" ]]; then
 			echo "󰌑 Return"
 		else
 			yt-dlp --flat-playlist "$BASE$2/$3" --print url --playlist-items $NUM_SELECTED
 		fi
 	else
-		( echo "󰌑 Return"; echo "󰐑 See all"; echo "󰲹 Music mode all"; echo "󰇚 Download all"; echo "󰐒 Save List"; yt-dlp --flat-playlist "$2" --print "%(playlist_index)d: %(title)s" ) | fzf --header "Select the desired videos"
+		NUM_SELECTED=$(( echo "󰌑 Return"; echo "󰐑 See all"; echo "󰲹 Music mode all"; echo "󰇚 Download all"; echo "󰐒 Save list"; yt-dlp --flat-playlist "$2" --print "%(playlist_index)d: %(title)s" ) | fzf --style full --header "Select the desired videos" | awk -F: '{print $1}')
+		if [[ "$NUM_SELECTED" != '󰌑 Return' ]] || [[ "$NUM_SELECTED" != "󰐑 See all" ]] || [[ "$NUM_SELECTED" != "󰲹 Music mode all" ]] || [[ "$NUM_SELECTED" != "󰇚 Download all" ]] || [[ "$NUM_SELECTED" != "󰐒 Save list" ]]; then
+			yt-dlp --flat-playlist "$2" --print url --playlist-items $NUM_SELECTED
+		fi
 	fi
 }
 
 type_media_search_menu()
 {
-	menu "videos\nplaylists\nstreams\nshorts" "--header 'What content do you want?'"
+	echo -en "󰌑 Return\n videos\n󰐑 playlists\n streams\n shorts"  | fzf --style full --header 'What content do you want?' | awk '{print $2}'
 }
 
 subscribed_menu()
@@ -35,18 +38,36 @@ subscribed_menu()
 	local TYPE_MEDIA_SEARCH_SELECTED=-1
 	local SUBSCRIBED_CHANNELS=$SUBSCRIBED_CHANNELS$(echo "󰌑 Return";cat $PATH_SUBSCRIBED_CHANNELS)
 	while [[ "$SUBSCRIBED_CHANNEL_SELECTED" != "󰌑 Return" ]]; do
-		SUBSCRIBED_CHANNEL_SELECTED=$(menu "$SUBSCRIBED_CHANNELS" "--header \"Select channel to see or add them in path=$PATH_SUBSCRIBED_CHANNELS\"")
+		SUBSCRIBED_CHANNEL_SELECTED=$(menu "$SUBSCRIBED_CHANNELS" "--header \"Select channel to see or add them in path=$PATH_SUBSCRIBED_CHANNELS\"" 1)
 		if [[ "$SUBSCRIBED_CHANNEL_SELECTED" != "󰌑 Return" ]]; then
-			TYPE_MEDIA_SEARCH_SELECTED=$(type_media_search_menu)
-			if [[ "$TYPE_MEDIA_SEARCH_SELECTED" == playlists ]]; then
-				local URL_PLAYLIST=-1
-				while [[ "$URL_PLAYLIST" != "󰌑 Return" ]]; do
-					URL_PLAYLIST=$(shift; get_youtube 0 $SUBSCRIBED_CHANNEL_SELECTED $TYPE_MEDIA_SEARCH_SELECTED)
-					if [[ "$URL_PLAYLIST" != "󰌑 Return" ]]; then
-						shift; get_youtube 1 "$URL_PLAYLIST"
-					fi
-				done
-			fi
+			TYPE_MEDIA_SEARCH_SELECTED=-1
+			while [[ $TYPE_MEDIA_SEARCH_SELECTED != "Return" ]]; do
+				TYPE_MEDIA_SEARCH_SELECTED=$(type_media_search_menu)
+				if [[ "$TYPE_MEDIA_SEARCH_SELECTED" == playlists ]]; then
+					local URL_PLAYLIST=-1
+					while [[ "$URL_PLAYLIST" != "󰌑 Return" ]]; do
+						URL_PLAYLIST=$(shift; get_youtube 0 $SUBSCRIBED_CHANNEL_SELECTED $TYPE_MEDIA_SEARCH_SELECTED)
+						if [[ "$URL_PLAYLIST" != "󰌑 Return" ]]; then
+							shift; get_youtube 1 "$URL_PLAYLIST"
+						fi
+					done
+				elif [[ $TYPE_MEDIA_SEARCH_SELECTED != "Return" ]]; then
+					local URL=-1
+					while [[ "$URL" != "󰌑 Return" ]]; do
+						URL=$(shift; get_youtube 0 $SUBSCRIBED_CHANNEL_SELECTED $TYPE_MEDIA_SEARCH_SELECTED)
+						if [[ "$URL" != "󰌑 Return" ]]; then							
+							local OPTION=$(echo -en "󰌑 Return\n See\n Music mode\n󰇚 Download\n󰐒 Save to list" | fzf --style full --header 'Video options...')
+							if [[ "$OPTION" == " See" ]]; then
+								# Save History
+								mpv --fullscreen "$URL"
+							elif [[ "$OPTION" == " Music mode" ]]; then
+								mpv --no-video "$URL"
+							fi
+						fi
+					done
+				fi
+			done
+
 		fi
 		
 	done
@@ -54,7 +75,7 @@ subscribed_menu()
 
 main_menu()
 {
-	shift; menu "0:  News!\n1: 󰵀 Subscribed\n2:  Stored\n3:  Settings\n4:  Close" "--header 'Select an option'"
+	shift; menu "0:  News!\n1: 󰵀 Subscribed\n2:  Stored\n3:  Settings\n4:  Close" "--header 'Select an option'" 1
 }
 
 while [[ "$MAIN_MENU" -ne "4" ]]; do

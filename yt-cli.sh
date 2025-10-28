b12a40c0434103e219558af59cb26bbcafd37d65
RacoonByte01 <javier212002@gmail.com>
2025-10-28 16:32:41 +0100

    docs(yt-cli): documenting the project - an explanatory comment was added for each functio - a to-do item was added

--- yt-cli.sh
+++ yt-cli.sh
@@ -4,11 +4,19 @@ PATH_SUBSCRIBED_CHANNELS=./subscribed_channels
 MAIN_MENU=-1
 BASE='https://www.youtube.com/@'
 
+# Make a menu by:
+# * $1: Menu items separated by `\n`.
+# * $2: Get the flags that fzf will have.
+# * $3: The desired menu item separated by `:`.
+# * Return: The number menu selected.
+#  TODO: Make the menus that load from yt-dpl load with the function for the loading bar.
 menu()
 {
 	echo -en "$1" | sh -c "fzf --style full $2" | awk -F: '{printf $'$3'}'
 }
 
+# Allows you to open a video from a URL.
+# Returns the video player process with the URL
 open_video()
 {
 	local OPTION=$(echo -en "󰌑 Return\n See\n Music mode\n󰇚 Download\n󰐒 Save to list" | fzf --style full --header 'Video options...')
@@ -20,6 +28,11 @@ open_video()
 	fi
 }
 
+# Allows you to search for content on YouTube.
+# * $1 Type of search 0==content{chanel:video, chanel:playlist, ...}; 1==URL.
+# * $2 | $1==0 name of chanel | $2==1 URL.
+# * $3 | $1==0 type content {video, playlist, ...} | $1==1 nothing.
+# Return URL of video.
 get_youtube()
 {
 	if [[ $1 -eq 0 ]]; then
@@ -39,11 +52,15 @@ get_youtube()
 	fi
 }
 
+# Create a menu to know what type of content you want from a channel.
+# Returns the content you want to see {videos, playlists, ...}.
 type_media_search_menu()
 {
 	echo -en "󰌑 Return\n videos\n󰐑 playlists\n streams\n shorts"  | fzf --style full --header 'What content do you want?' | awk '{print $2}'
 }
 
+# Perform all necessary actions to obtain the video playlist ... from a subscribed channel.
+# Returns the video player process with the desired URL.
 subscribed_menu()
 {
 	[[ -f $PATH_SUBSCRIBED_CHANNELS ]] || touch $PATH_SUBSCRIBED_CHANNELS
@@ -56,6 +73,7 @@ subscribed_menu()
 			TYPE_MEDIA_SEARCH_SELECTED=-1
 			while [[ $TYPE_MEDIA_SEARCH_SELECTED != "Return" ]]; do
 				TYPE_MEDIA_SEARCH_SELECTED=$(type_media_search_menu)
+				#  INFO: Playlist search mode.
 				if [[ "$TYPE_MEDIA_SEARCH_SELECTED" == playlists ]]; then
 					local URL_PLAYLIST=-1
 					while [[ "$URL_PLAYLIST" != "󰌑 Return" ]]; do
@@ -70,6 +88,7 @@ subscribed_menu()
 							done
 						fi
 					done
+				#  INFO: Direct video, short, stream search mode.
 				elif [[ $TYPE_MEDIA_SEARCH_SELECTED != "Return" ]]; then
 					local URL=-1
 					while [[ "$URL" != "󰌑 Return" ]]; do
@@ -84,11 +103,14 @@ subscribed_menu()
 	done
 }
 
+# Create a menu to find out what type of search the user wants to perform.
+# Return the number of seelecction {0..4}.
 main_menu()
 {
 	shift; menu "0:  News!\n1: 󰵀 Subscribed\n2:  Stored\n3:  Settings\n4:  Close" "--header 'Select an option'" 1
 }
 
+# Run the script until it stops.
 while [[ "$MAIN_MENU" -ne "4" ]]; do
 	MAIN_MENU=$(main_menu)
 	case "$MAIN_MENU" in

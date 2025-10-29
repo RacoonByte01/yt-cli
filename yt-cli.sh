bfdf3e00fd5621288f32cd7aeedeb48216f1a328
RacoonByte01 <javier212002@gmail.com>
2025-10-29 18:26:50 +0100

    feat(scope): can return to the previous menu with `ctr+q`

--- yt-cli.sh
+++ yt-cli.sh
@@ -37,18 +37,27 @@ get_youtube()
 {
 	if [[ $1 -eq 0 ]]; then
 		NUM_SELECTED=$(fzf --style full --header "Select the desired $3" --bind "start,ctrl-r:reload:echo \"󰌑 Return\"; i=0; while [[ \"\$i\" != \"-1\" ]]; do feed=\$(yt-dlp -i -q --flat-playlist \"$BASE$2/$3\" --playlist-start \$((\$i*100+1)) --playlist-end \$((\$i*100+100)) --print \"%(playlist_index)d: %(title)s\"); i=\$((\$i+1)); [[ \"\$feed\" != \"\" ]] && echo \$feed; [[ \"\$(echo \$feed | wc -l)\" -lt \"100\" ]] && i=-1; done" | awk -F: '{print $1}')
-		if [[ $NUM_SELECTED == "󰌑 Return" ]]; then
-			echo "󰌑 Return"
-		else
-			yt-dlp -q --ignore-errors --no-warnings --flat-playlist "$BASE$2/$3" --print url --playlist-items "$NUM_SELECTED" 2> /dev/null
-		fi
+		case "$NUM_SELECTED" in
+			"󰌑 Return"|"")
+				echo "󰌑 Return"
+			;;
+			*)
+				yt-dlp -q --ignore-errors --no-warnings --flat-playlist "$BASE$2/$3" --print url --playlist-items "$NUM_SELECTED" 2> /dev/null
+			;;
+		esac
 	else
 		NUM_SELECTED=$( ( echo "󰌑 Return"; echo "󰐑 See all"; echo "󰲹 Music mode all"; echo "󰇚 Download all"; echo "󰐒 Save list"; yt-dlp -q --ignore-errors --no-warnings --flat-playlist "$2" --print "%(playlist_index)d: %(title)s" 2> /dev/null ) | fzf --style full --header "Select the desired videos" | awk -F: '{print $1}')
-		if [ "$NUM_SELECTED" = '󰌑 Return' ] || [ "$NUM_SELECTED" = "󰐑 See all" ] || [ "$NUM_SELECTED" = "󰲹 Music mode all" ] || [ "$NUM_SELECTED" = "󰇚 Download all" ] || [ "$NUM_SELECTED" = "󰐒 Save list" ]; then
-			echo "$NUM_SELECTED"
-		else
-			yt-dlp -q --ignore-errors --no-warnings --flat-playlist "$2" --print url --playlist-items "$NUM_SELECTED" 2> /dev/null
-		fi
+		case "$NUM_SELECTED" in
+			"󰐑 See all"|"󰲹 Music mode all"|"󰇚 Download all"|"󰐒 Save list")
+				echo "$NUM_SELECTED"
+				;;
+			"󰌑 Return"|"")
+				echo "󰌑 Return"
+				;;
+			*)
+				yt-dlp -q --ignore-errors --no-warnings --flat-playlist "$2" --print url --playlist-items "$NUM_SELECTED" 2> /dev/null
+				;;
+		esac
 	fi
 }
 
@@ -56,7 +65,7 @@ get_youtube()
 # Returns the content you want to see {videos, playlists, ...}.
 type_media_search_menu()
 {
-	echo -en "󰌑 Return\n videos\n󰐑 playlists\n streams\n shorts"  | fzf --style full --header 'What content do you want?' | awk '{print $2}'
+	echo -en "󰌑 Return\n videos\n󰐑 playlists\n streams\n shorts" | fzf --style full --header 'What content do you want?' | awk '{print $2}'
 }
 
 # Perform all necessary actions to obtain the video playlist ... from a subscribed channel.
@@ -66,37 +75,41 @@ subscribed_menu()
 	[[ -f $PATH_SUBSCRIBED_CHANNELS ]] || touch $PATH_SUBSCRIBED_CHANNELS
 	SUBSCRIBED_CHANNEL_SELECTED=-1
 	SUBSCRIBED_CHANNELS=$SUBSCRIBED_CHANNELS$(echo "󰌑 Return";cat $PATH_SUBSCRIBED_CHANNELS)
-	while [[ "$SUBSCRIBED_CHANNEL_SELECTED" != "󰌑 Return" ]]; do
+	while ! { [ "$SUBSCRIBED_CHANNEL_SELECTED" == "󰌑 Return" ] || [ "$SUBSCRIBED_CHANNEL_SELECTED" == "" ]; }; do
 		SUBSCRIBED_CHANNEL_SELECTED=$(menu "$SUBSCRIBED_CHANNELS" "--header \"Select channel to see or add them in path=$PATH_SUBSCRIBED_CHANNELS\"" 1)
-		if [[ "$SUBSCRIBED_CHANNEL_SELECTED" != "󰌑 Return" ]]; then
+		if ! { [ "$SUBSCRIBED_CHANNEL_SELECTED" == "󰌑 Return" ] || [ "$SUBSCRIBED_CHANNEL_SELECTED" == "" ]; }; then
 			TYPE_MEDIA_SEARCH_SELECTED=-1
 			while [[ $TYPE_MEDIA_SEARCH_SELECTED != "Return" ]]; do
 				TYPE_MEDIA_SEARCH_SELECTED=$(type_media_search_menu)
-				#  INFO: Playlist search mode.
-				if [[ "$TYPE_MEDIA_SEARCH_SELECTED" == playlists ]]; then
-					local URL_PLAYLIST=-1
-					while [[ "$URL_PLAYLIST" != "󰌑 Return" ]]; do
-						URL_PLAYLIST=$(shift; get_youtube 0 "$SUBSCRIBED_CHANNEL_SELECTED" "$TYPE_MEDIA_SEARCH_SELECTED")
-						if [[ "$URL_PLAYLIST" != "󰌑 Return" ]]; then
-							local URL=-1
-							while [[ "$URL" != "󰌑 Return" ]]; do
-								URL=$(get_youtube 1 "$URL_PLAYLIST")
-								if [[ "$URL" != "󰌑 Return" ]]; then
-									shift; open_video "$URL"
-								fi
-							done
-						fi
-					done
-				#  INFO: Direct video, short, stream search mode.
-				elif [[ $TYPE_MEDIA_SEARCH_SELECTED != "Return" ]]; then
-					local URL=-1
-					while [[ "$URL" != "󰌑 Return" ]]; do
-						URL=$(shift; get_youtube 0 "$SUBSCRIBED_CHANNEL_SELECTED" "$TYPE_MEDIA_SEARCH_SELECTED")
-						if [[ "$URL" != "󰌑 Return" ]]; then							
-							shift; open_video "$URL"
-						fi
-					done
-				fi
+				case "$TYPE_MEDIA_SEARCH_SELECTED" in
+					"Return"|"")
+						TYPE_MEDIA_SEARCH_SELECTED="Return"
+						;;
+					"playlists")
+						local URL_PLAYLIST=-1
+						while [[ "$URL_PLAYLIST" != "󰌑 Return" ]]; do
+							URL_PLAYLIST=$(shift; get_youtube 0 "$SUBSCRIBED_CHANNEL_SELECTED" "$TYPE_MEDIA_SEARCH_SELECTED")
+							if [[ "$URL_PLAYLIST" != "󰌑 Return" ]]; then
+								local URL=-1
+								while [[ "$URL" != "󰌑 Return" ]]; do
+									URL=$(get_youtube 1 "$URL_PLAYLIST")
+									if [[ "$URL" != "󰌑 Return" ]]; then
+										shift; open_video "$URL"
+									fi
+								done
+							fi
+						done
+						;;
+					*)
+						local URL=-1
+						while [[ "$URL" != "󰌑 Return" ]]; do
+							URL=$(shift; get_youtube 0 "$SUBSCRIBED_CHANNEL_SELECTED" "$TYPE_MEDIA_SEARCH_SELECTED")
+							if [[ "$URL" != "󰌑 Return" ]]; then							
+								shift; open_video "$URL"
+							fi
+						done
+						;;
+				esac
 			done
 		fi
 	done
@@ -110,7 +123,7 @@ main_menu()
 }
 
 # Run the script until it stops.
-while [[ "$MAIN_MENU" -ne "4" ]]; do
+while ! { [ "$MAIN_MENU" == "4" ] || [ "$MAIN_MENU" == "" ]; }; do
 	MAIN_MENU=$(main_menu)
 	case "$MAIN_MENU" in
 		1) shift; subscribed_menu
